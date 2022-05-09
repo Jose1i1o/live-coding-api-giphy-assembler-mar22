6a7bebefbe1879310b0d73f0633b9e7412d7693c
Jose1i1o <guadix88@gmail.com>
2022-05-09 16:49:43 +0200

    add callback examples

--- src/js/main.js
+++ src/js/main.js
@@ -1,73 +1,23 @@
-const apiKey = "EhdRmrgVkNVGNw9aYoKS90hAJgBbPYz5";
+function firstFunction(callback) {
+  console.log("This is the first function");
+  setTimeout(callback, 1000)
+}
 
-const fetchApi =
-fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+function secondFunction() {
+  console.log("This is the second function");
+}
 
+setTimeout(() => firstFunction(() => secondFunction()), 4000);
 
-// arrow function
-fetchApi
-.then(response => response.json())
-.then(data => {
-  const title = data.data.title;
-  const url = data.data.images.original.url;
+// end step
+function firstFunction(callback, message) {
+  console.log(message);
+  setTimeout(callback, 1000)
+}
 
-  const titleElement = document.createElement("h2");
-  const img = document.createElement("img");
+function secondFunction(message) {
+  console.log(message);
+}
 
-  titleElement.textContent = title;
-  img.src = url;
+setTimeout(() => firstFunction(() => secondFunction("This is the second Function callback"), "I am the first Function Callback"), 4000);
 
-  const printTitle = document.getElementById("title");
-  const printImage = document.getElementById("posts");
-
-  printTitle.append(titleElement);
-  printImage.append(img);
-})
-.catch(error => console.log(error));
-
-// with functions and not arrow functions
-fetchApi
-.then(function (response) {
-  return response.json();
-})
-.then(function (data) {
-  const title = data.data.title;
-  const url = data.data.images.original.url;
-
-  const titleElement = document.createElement("h2");
-  const img = document.createElement("img");
-
-  titleElement.textContent = title;
-  img.src = url;
-
-  const printTitle = document.getElementById("title");
-  const printImage = document.getElementById("posts");
-
-  printTitle.append(titleElement);
-  printImage.append(img);
-})
-.catch(function (error) {
-  console.log(error);
-});
-
-// const getImage = async () => { // async returns the value by default in a promise
-
-//   try {
-
-//   const apiKey = 'EhdRmrgVkNVGNw9aYoKS90hAJgBbPYz5';
-//   const response = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`); // if you hover over the petition you will see it´s a response type of variable. So it will wait for the fetch to be done, before passing to the next argument, function, variable etc.
-//   const { data } = await response.json(); // wait for the response to be completed
-
-//   const { url } = data.images.original;
-//   console.log(url);
-//   const img = document.createElement('img');
-//   img.src = url;
-
-//   document.body.append(img)
-
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-// getImage();
